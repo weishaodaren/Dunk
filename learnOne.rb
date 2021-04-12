30603cd3293cbfa08aba6b4744df13fa38e2abba
weishaodaren <weishaodaren@gmail.com>
2021-04-12 10:38:19 +0800

    here doc

--- learnOne.rb
+++ learnOne.rb
@@ -1,1 +1,20 @@
 puts 'Hello Ruby'
+
+print <<EOF
+    by order of peaky blinders
+EOF
+
+print <<'EOF'
+    by order of peaky blinders
+EOF
+
+print <<`EOC`
+    echo hi there
+    echo lo there
+EOC
+
+print <<'foo', <<'bar'
+    I said foo.
+foo
+    I said bar.
+bar
