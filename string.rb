450f0f04021786ec173b76d8433fd58b22d6e9c0
weishaodaren <weishaodaren@gmail.com>
2021-04-14 11:54:40 +0800

    slowly update String methods

--- string.rb
+++ string.rb
@@ -2,8 +2,8 @@
 
 =begin
 
-downcase capitalize upcase 
-downcase! capitalize! upcase! 修改本身
+downcase capitalize upcase chomp
+downcase! capitalize! upcase! chomp! 修改本身
 
 =end
 
@@ -18,9 +18,10 @@ foo = my_str[0 ... 2]         # 根据索引获取
 
 puts foo
 
-ur_string = String.new('this is string')
+ur_string = String.new("this is string\n")
 bar = ur_string.capitalize!
 bar = bar.casecmp(ur_string)  # 不区分大小写比较 -1 0 1
-bar = ur_string.center(100, 'asd') # 居中 不够的 填充
+bar = ur_string.chomp.center(100, 'asd') #  居中 不够的 填充
+bar = ur_string.chomp # 去掉末尾的转义字符
 
 puts bar
