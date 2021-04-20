2c6d53e089e6ab289e2bb7e39fac44cd640a352f
weishaodaren <weishaodaren@gmail.com>
2021-04-20 11:01:53 +0800

    real smarter than JS

--- array.rb
+++ array.rb
@@ -1,14 +1,16 @@
 names = Array.new(20)
-p names.length + names.size
 
 names = Array.new(10) { |e| e = e * 2}
-p names
 
-names = Array.[](1, 2, 3, 4, 5)
-p names
-
-names = Array[1, 2, 3, 4, 5]
-p names
+names = Array.[](1, 2, 3, 4, 5)  #  names = Array[1, 2, 3, 4, 5]
 
 names = Array(0 ... 9).at(6)
-p names
+
+ary = Array.[](1, 2, 3)
+ary = ary & [2]                             # & 交集
+ary = ary * 10                              # 返回长度 * int 的数组
+ary = ary * 'wei'                           # 返回拼接的字符串
+ary = [1, 2, 4] + [3]                       # 数组拼接
+ary = ary - [3, 23, 4]                      # 数组去除指定的参数
+
+p ary
