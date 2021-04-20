d21b57693f70891f5ec6fca02a9f1f5e0486ce9a
weishaodaren <weishaodaren@gmail.com>
2021-04-20 11:29:29 +0800

    update array api

--- array.rb
+++ array.rb
@@ -7,10 +7,13 @@ names = Array.[](1, 2, 3, 4, 5)  #  names = Array[1, 2, 3, 4, 5]
 names = Array(0 ... 9).at(6)
 
 ary = Array.[](1, 2, 3)
-ary = ary & [2]                             # & 交集
-ary = ary * 10                              # 返回长度 * int 的数组
-ary = ary * 'wei'                           # 返回拼接的字符串
-ary = [1, 2, 4] + [3]                       # 数组拼接
-ary = ary - [3, 23, 4]                      # 数组去除指定的参数
+ary = ary & [2]                                           # & 交集
+ary = ary * 10                                            # 返回长度 * int 的数组
+ary = ary * 'wei'                                         # 返回拼接的字符串
+ary = [1, 2, 4] + [3]                                     # 数组拼接
+ary = ary - [3, 23, 4]                                    # 数组去除指定的参数
+ary = ary | [90, 100, 2, 1, 1, 2, 90]                     # 返回一个去重的数组
+ary = ary << {name: 'weisha0daren'} << 90 << [[[2]]]      # 数组末尾插入参数
+ary = Array.[](100)*100 <=> ary
 
 p ary
